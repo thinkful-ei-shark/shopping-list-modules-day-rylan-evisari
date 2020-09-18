80faad26e844debecd48fc8be696a4ff83a82110
Evisari Riyanto <evisari111012@gmail.com>
2020-09-18 14:31:55 -0700

    step 5

--- scripts/store.js
+++ scripts/store.js
@@ -3,8 +3,47 @@ import item from './item.js';
 const items = []; 
 const hideCheckedItems = false;
 
+const findById = function (id) {
+ return store.items.find(item => item.id === id);
+}
+
+const addItems = function (name) {
+    try {
+        item.validateName(itemName);
+        this.items.push(item.create(itemName));
+        //item.create(itemName);
+      } catch (error) {
+        console.log(`Cannot add item: ${error.message}`);
+      }
+}
+
+const findAndToggleChecked = function(id) {
+    const myId = this.findById(); 
+    myId.checked = !myId.checked;
+}
+
+const findAndUpdateName = function(id, newName) {
+    try {
+        validateName(newName);
+        findById(id); 
+    } catch(error) {
+        console.log(`Cannot update name: ${error.message}`);
+    }
+}
+
+const findAndDelete = function (id) {
+    const index = this.items.findIndex(item => item.id === id);
+    this.items.splice(index, 1);
+}   
+
+
 
 export default {
+    addItems, 
+    findById,
+    findAndToggleChecked,
+    findAndDelete,
+    findAndUpdateName,
     items,
     hideCheckedItems
   };
